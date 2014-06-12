0bf0f8db05dece7d2e082fd47b4d26e686476f47
David Ehlen <dehlen@me.com>
2014-06-12 16:51:21 +0200

    Gültige Email, Alle Felder ausfüllen Fehlermeldungen auf Loginscreen, falsche Anmeldedaten etc folgen in ErrorDialog

--- 4. Implementierungsphase/Eclipse Workspace/flashcard-website/WebContent/index.jsp
+++ 4. Implementierungsphase/Eclipse Workspace/flashcard-website/WebContent/index.jsp
@@ -4,14 +4,18 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="layout/css/layout.css">
     <link href='http://fonts.googleapis.com/css?family=Megrim' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+  <script src="js/jquery.growl.js" type="text/javascript"></script>
+  <link href="layout/css/jquery.growl.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript" src="js/login.js"></script>
     
+  
 </head>
 
 <body>
 	<div class="login-holder">
 	    <h1>StudyFlash</h1>
-	    <form id="loginForm" name="loginForm" class="loginForm" method=post action="jsp/loginDB.jsp">
+	    <form id="loginForm" name="loginForm" class="loginForm" method=post action="javascript:void(0);" onsubmit="return checkForm()">
 	        <label id="formHeadline" class="loginLabel">Loginscreen</label>
 	            <input type="text" id="login" name="uname" placeholder="Username" spellcheck="false">
 	            <input type="password" name="password" id="password" placeholder="Password" spellcheck="false">
--- 4. Implementierungsphase/Eclipse Workspace/flashcard-website/WebContent/js/login.js
+++ 4. Implementierungsphase/Eclipse Workspace/flashcard-website/WebContent/js/login.js
@@ -1,20 +1,57 @@
 function toRegister() {
-    if (document.getElementById("loginId").value === "Login") {
-        document.getElementById("loginId").value = "Registrieren";
+    var login = document.getElementById("loginId");
+    if (login.value === "Login") {
+        login.value = "Registrieren";
 
         document.getElementById("register").innerHTML = "Zurueck zum Login...";
         document.getElementById("formHeadline").innerHTML = "Signupscreen";
-        
+
         document.loginForm.action = "jsp/registerDB.jsp";
 
     } else {
 
-        document.getElementById("loginId").value = "Login";
+        login.value = "Login";
 
         document.getElementById("register").innerHTML = "Noch keinen Account ?";
         document.getElementById("formHeadline").innerHTML = "Loginscreen";
-        
+
         document.loginForm.action = "jsp/loginDB.jsp";
 
+    }
+}
+
+function setCorrectAction() {
+    var login = document.getElementById("loginId");
+    if (login.value === "Login") {
+
+        document.loginForm.action = "jsp/loginDB.jsp";
+
+    } else {
+
+        document.loginForm.action = "jsp/registerDB.jsp";
+
+    }
+}
+
+
+
+function checkForm() {
+    var x = document.getElementById("login").value;
+    var passField = document.getElementById("password").value;
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    setCorrectAction();
+
+    if (regex.test(x) == false) {
+        document.loginForm.action = "javascript: void(0);"
+        $.growl.error({
+            message: "Email Adresse ist nicht valide"
+        });
+
+    }
+    if(!passField || passField.length == 0) {
+       document.loginForm.action = "javascript: void(0);"
+        $.growl.error({
+            message: "Bitte ein Passwort eingeben"
+        }); 
     }
 }

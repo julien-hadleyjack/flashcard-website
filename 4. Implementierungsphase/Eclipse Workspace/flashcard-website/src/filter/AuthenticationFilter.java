4ceaa3e2383f0015b4e428df6a763ed1ed9ecd55
David Ehlen <dehlen@me.com>
2014-06-14 20:53:10 +0200

    Seiten nur noch als eingeloggter User aufrufbar, ist man nicht eingeloggt wird man zum Loginscreen geleitet

---
+++ 4. Implementierungsphase/Eclipse Workspace/flashcard-website/src/filter/AuthenticationFilter.java
@@ -0,0 +1,37 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthenticationFilter implements Filter {
+
+
+		  @Override
+		  public void init(FilterConfig fc) throws ServletException {
+		  }
+
+		  @Override
+		  public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc) throws IOException, ServletException {
+		    HttpServletRequest req = (HttpServletRequest) request;
+		    HttpServletResponse res = (HttpServletResponse) response;  
+
+		    if (req.getSession().getAttribute("userid") == null) {
+		        res.sendRedirect("/index.jsp");
+		    } else {		     
+		      fc.doFilter(request, response);
+		    }
+		  }
+
+		  @Override
+		  public void destroy() {
+		  }
+
+}
